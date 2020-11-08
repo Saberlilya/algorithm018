83bd4828abfe3c31b74993707c5d125a9d107974
Saberlilya <48541916+Saberlilya@users.noreply.github.com>
2020-11-08 20:51:10 +0800

    week03

--- week03/NOTE.md
+++ week03/NOTE.md
@@ -1,1 +1,15 @@
 学习笔记
+
+泛型递归
+https://www.yuque.com/saberlily/pkeog2/ggc44e
+
+分治回溯
+https://www.yuque.com/saberlily/pkeog2/oqp5r4
+
+二叉树最近公共祖先
+https://www.yuque.com/saberlily/cthdh8/urv4th
+
+从前序中序 构造二叉树
+https://www.yuque.com/saberlily/cthdh8/ac3g0x
+
+最近忙部分科目期末 之后能做到更好一点。
---
+++ week03/二叉树最近公共祖先.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root==p||root==q) {
+            return root;
+        }
+        if (root!=null){
+            TreeNode lNode=lowestCommonAncestor(root.left,p,q);
+            TreeNode rNode=lowestCommonAncestor(root.right,p,q);
+            if (lNode!=null&&rNode!=null)
+                return root;
+            else if(lNode==null) {//两个都在右子树
+                return rNode;
+            }
+            else { //两个都在左子树里面
+                return lNode;
+            }
+        }
+        return null;
+    }
+}
